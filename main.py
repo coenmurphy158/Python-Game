440c0dd1eab1fc4e00f252f8ce815d5f84c6713e
coenmurphy <coenmurphy158@gmail.com>
2026-05-07 13:32:27 -0400

    fixed a lot of things like organization and funny comment

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 import pygame
 import sys
-import pygame_menu
 import random
-#dih
-pygame.init()
 
 WIDTH, HEIGHT = 800, 600
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-spawn_enemy = pygame.USEREVENT + 1
-pygame.time.set_timer(spawn_enemy,3000)
+char_x = WIDTH // 2
+char_y = HEIGHT // 2
+char_speed = 2
 
-menu = pygame_menu.Menu('Main Menu', 600, 400)
-menu.add.button('Play', lambda: menu.disable())
-menu.add.button('Quit', pygame_menu.events.EXIT)
-menu.mainloop(win)
 
-player = pygame.image.load('aircraft.png').convert_alpha()
-laser_image = pygame.image.load('laser.gif').convert_alpha()
-enemy_img = pygame.image.load('enemypixel.gif').convert_alpha()
-background = pygame.image.load("sky.png").convert()
-background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+
 
 laser_speed = 5
 laser_list = []
@@ -30,9 +19,17 @@ bg_y1 = 0
 bg_y2 = -HEIGHT
 bg_speed = 1
 
+#double shot powerup
+class Powerup:
+    def __init__(self):
+        self.sprite = pygame.image.load('powerup2.bmp').convert_alpha()
+        self.rect = pygame.rect.Rect(self.sprite.get_rect().center)
+    def draw(self):
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
+
 class Enemy:
-    walkRight = [pygame.image.load('enemypixel.gif')]
-    walkLeft = [pygame.image.load('enemypixel.gif')]
+    walkRight = [pygame.image.load('enemypixel.bmp')]
+    walkLeft = [pygame.image.load('enemypixel.bmp')]
 
     def __init__(self, x, y, width, height, end):
         self.x = x
@@ -41,7 +38,7 @@ class Enemy:
         self.height = height
         self.path = [x, end]
         self.vel = 4
-        self.image = pygame.image.load('enemypixel.gif').convert_alpha()
+        self.image = pygame.image.load('enemypixel.bmp').convert_alpha()
 
     def draw(self):
         win.blit(self.image, (self.x, self.y))
@@ -65,77 +62,83 @@ def get_sprite(sheet, col, row):
     rect = pygame.Rect(col * SPRITE_W, row * SPRITE_H, SPRITE_W, SPRITE_H)
     return sheet.subsurface(rect)
 
-char_x = WIDTH // 2
-char_y = HEIGHT // 2
-char_speed = 2
+if __name__ == '__main__':
+    pygame.init()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    spawn_enemy = pygame.USEREVENT + 1
+    pygame.time.set_timer(spawn_enemy, 3000)
+
+    player = pygame.image.load('aircraft.bmp').convert_alpha()
+    laser_image = pygame.image.load('laser.bmp').convert_alpha()
+    enemy_img = pygame.image.load('enemypixel.bmp').convert_alpha()
+    background = pygame.image.load("sky.bmp").convert()
+
+    background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == spawn_enemy:
+                x = random.randint(0, WIDTH - 20)
+                enemies.append(Enemy(x, 0, 30, 30, x + 200))
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            char_x -= char_speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            char_x += char_speed
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            char_y -= char_speed
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            char_y += char_speed
+        if keys[pygame.K_ESCAPE]:
             pygame.quit()
-            sys.exit()
-        if event.type == spawn_enemy:
-            x = random.randint(0, WIDTH - 20)
-            enemies.append(Enemy(x, 0, 30, 30, x + 200))
 
-    keys = pygame.key.get_pressed()
+        char_x = max(0, min(WIDTH - 16, char_x))
+        char_y = max(0, min(HEIGHT - 16, char_y))
 
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        char_x -= char_speed
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        char_x += char_speed
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        char_y -= char_speed
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        char_y += char_speed
+        bg_y1 += bg_speed
+        bg_y2 += bg_speed
+        if bg_y1 >= HEIGHT:
+            bg_y1 = -HEIGHT
+        if bg_y2 >= HEIGHT:
+            bg_y2 = -HEIGHT
 
-    char_x = max(0, min(WIDTH - 16, char_x))
-    char_y = max(0, min(HEIGHT - 16, char_y))
+        win.blit(background, (0, bg_y1))
+        win.blit(background, (0, bg_y2))
 
-    bg_y1 += bg_speed
-    bg_y2 += bg_speed
-    if bg_y1 >= HEIGHT:
-        bg_y1 = -HEIGHT
-    if bg_y2 >= HEIGHT:
-        bg_y2 = -HEIGHT
+        player_image = get_sprite(player, 3, 3)
+        win.blit(player_image, (char_x, char_y))
 
-    win.blit(background, (0, bg_y1))
-    win.blit(background, (0, bg_y2))
+        laser_counter += 1
+        if laser_counter >= 15:
+            laser_counter = 0
 
-    player_image = get_sprite(player, 3, 3)
-    win.blit(player_image, (char_x, char_y))
+        if keys[pygame.K_SPACE] and laser_counter == 0:
+            laser_list.append({"laser_rect": pygame.Rect(char_x + 7, char_y, 2, 8)})
+            laser_counter = 1
 
-    laser_counter += 1
-    if laser_counter >= 15:
-        laser_counter = 0
+        for laser in laser_list:
+            pygame.draw.rect(win, (255, 255, 255), laser["laser_rect"])
+            laser["laser_rect"].y -= laser_speed
 
-    if keys[pygame.K_SPACE] and laser_counter == 0:
-        laser_list.append({"laser_rect": pygame.Rect(char_x + 7, char_y, 2, 8)})
-        laser_counter = 1
+        laser_list = [laser for laser in laser_list if laser["laser_rect"].y > -10]
 
-    for laser in laser_list:
-        pygame.draw.rect(win, (255, 255, 255), laser["laser_rect"])
-        laser["laser_rect"].y -= laser_speed
+        for laser in laser_list[:]:
+            for enemy in enemies[:]:
+                enemy_rect = pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)
+                if laser["laser_rect"].colliderect(enemy_rect):
+                    enemies.remove(enemy)
+                    laser_list.remove(laser)
+                    break
 
-    laser_list = [laser for laser in laser_list if laser["laser_rect"].y > -10]
+        for enemy in enemies:
+            enemy.draw()
 
-    for laser in laser_list[:]:
-        for enemy in enemies[:]:
-            enemy_rect = pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)
-            if laser["laser_rect"].colliderect(enemy_rect):
-                enemies.remove(enemy)
-                laser_list.remove(laser)
-                break
-
-    for enemy in enemies:
-        enemy.draw()
-
-#double shot powerup
-    class Powerup:
-        def __init__(self):
-            self.image = pygame.image.load('powerup2.png').convert_alpha()
-            self.rect = self.image()
-            self.rect.x = random.randint(300, 400)
-
-    pygame.display.update()
-    clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
